948ac6c76db66ca6994031cf4d58459eec40d576
Eric Rochester <erochest@virginia.edu>
2014-10-29 15:37:07 -0400

    Reformatting, especially whitespace ;)
    * No tabs!
    * Removed trailing whitespace.
    * Use 4 spaces for indentation.
    * Made my lint-checker happy by adding spaces after comments and
      wrapping lines.

--- punctuated_speech.py
+++ punctuated_speech.py
@@ -1,5 +1,4 @@
 from collections import Iterable
-import string
 
 # Pulls in the text
 f = open('Mrs.Dalloway.txt', 'r')
@@ -17,14 +16,14 @@ close_quote_indices = []
 characters_dict = dict(enumerate(characters_list))
 
 for key, value in characters_dict.items():
-	if value == "“":
-		open_quote_indices.append(key)
-	elif value == "”":
-		close_quote_indices.append(key)
-	else:
-		pass
+    if value == "“":
+        open_quote_indices.append(key)
+    elif value == "”":
+        close_quote_indices.append(key)
+    else:
+        pass
 
-#grabs number of punctuated phrases in novel. 
+# grabs number of punctuated phrases in novel.
 number_of_punctuated_phrases = len(open_quote_indices)
 
 punctuated_characters = []
@@ -33,25 +32,25 @@ punctuated_phrases = []
 ordered_punctuated_phrases = []
 
 
-# gathers the punctuated phrases into a list and stores them in ordered_punctuated_phrases 
-# which maintains their order in the novel.
+# gathers the punctuated phrases into a list and stores them in
+# ordered_punctuated_phrases which maintains their order in the novel.
 
 
 i = 0
-for i in range(0,number_of_punctuated_phrases):
-	phrase_start = open_quote_indices[i]
-	phrase_end = close_quote_indices[i]
-	phrase_characters = characters_list[phrase_start:phrase_end+1]
-	ordered_punctuated_phrases.append(phrase_characters)
+for i in range(0, number_of_punctuated_phrases):
+    phrase_start = open_quote_indices[i]
+    phrase_end = close_quote_indices[i]
+    phrase_characters = characters_list[phrase_start:phrase_end+1]
+    ordered_punctuated_phrases.append(phrase_characters)
 
-# compresses ordered_punctuated_phrases back together. previously it was just a big list
-# of individual characters.
+# compresses ordered_punctuated_phrases back together. previously it was just a
+# big list of individual characters.
 i = 0
 for item in ordered_punctuated_phrases:
-	ordered_punctuated_phrases[i] = "".join(item)
-	i+=1
+        ordered_punctuated_phrases[i] = "".join(item)
+        i += 1
 
-# getting a big unordered list of the different punctuated phrase characters.	
+# getting a big unordered list of the different punctuated phrase characters.
 
 try:
     unicode = unicode
@@ -60,7 +59,7 @@ except NameError:
     str = str
     unicode = str
     bytes = bytes
-    basestring = (str,bytes)
+    basestring = (str, bytes)
 else:
     # 'unicode' exists, must be Python 2
     str = str
@@ -70,18 +69,22 @@ else:
 
 raw_punctuated_phrases = []
 
+
 def flatten(lis):
-     for item in lis:
-         if isinstance(item, Iterable) and not isinstance(item, basestring):
-             for x in flatten(item):
-                 yield x
-         else:        
-             yield item
+    for item in lis:
+        if isinstance(item, Iterable) and not isinstance(item, basestring):
+            for x in flatten(item):
+                yield x
+        else:
+            yield item
+
 
 raw_punctuated_phrases = list(flatten(ordered_punctuated_phrases))
 raw_punctuated_phrases = "".join(raw_punctuated_phrases)
-# raw_punctuated phrases now has a string of all of the characters and punctuation marks. 
+
+# raw_punctuated phrases now has a string of all of the characters and
+# punctuation marks.
 
 # next thing to do - get rid of the punctuation marks?
-print(ordered_punctuated_phrases)	
+print(ordered_punctuated_phrases)
 print(raw_punctuated_phrases)
