8096c9ef09236982a99ce090f4ad69d1a3fe21c7
Eric Rochester <erochest@virginia.edu>
2014-10-29 16:22:52 -0400

    There's really nothing to flatten.
    Since we're getting a list of strings, you know.

--- punctuated_speech.py
+++ punctuated_speech.py
@@ -1,7 +1,6 @@
 
 
 import codecs
-from collections import Iterable
 import re
 
 
@@ -22,43 +21,7 @@ ordered_punctuated_phrases = []
 for (i, j) in zip(open_quote_indices, close_quote_indices):
     ordered_punctuated_phrases.append(clean_text[i:j+1])
 
-# compresses ordered_punctuated_phrases back together. previously it was just a
-# big list of individual characters.
-i = 0
-for item in ordered_punctuated_phrases:
-        ordered_punctuated_phrases[i] = "".join(item)
-        i += 1
-
-# getting a big unordered list of the different punctuated phrase characters.
-
-try:
-    unicode = unicode
-except NameError:
-    # 'unicode' is undefined, must be Python 3
-    str = str
-    unicode = str
-    bytes = bytes
-    basestring = (str, bytes)
-else:
-    # 'unicode' exists, must be Python 2
-    str = str
-    unicode = unicode
-    bytes = str
-    basestring = basestring
-
 raw_punctuated_phrases = []
-
-
-def flatten(lis):
-    for item in lis:
-        if isinstance(item, Iterable) and not isinstance(item, basestring):
-            for x in flatten(item):
-                yield x
-        else:
-            yield item
-
-
-raw_punctuated_phrases = list(flatten(ordered_punctuated_phrases))
 raw_punctuated_phrases = "".join(raw_punctuated_phrases)
 
 # raw_punctuated phrases now has a string of all of the characters and
