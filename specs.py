8dfe7881af49ee6ba18f3a8e41de6ca2c34e2ebc
Eric Rochester <erochest@virginia.edu>
2015-01-28 10:44:07 -0500

    Fixed the only spec.

--- specs.py
+++ specs.py
@@ -7,6 +7,7 @@ class TestFindQuotedQuotes:
 
     def test_it_should_find_double_quotes(self):
         quotes = [
-            m.group() for m in ps.find_quoted_quotes('She said, "Howdy!"')
+            m.group()
+            for m in ps.find_quoted_quotes('She said, "Howdy!" ' * 100)
             ]
-        assert quotes == ["Howdy!"]
+        assert quotes == (['"Howdy!"'] * 100), repr(quotes)
