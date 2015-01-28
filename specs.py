b7199d522cd8415cbec7e6e95b64b996cefa480a
Eric Rochester <erochest@virginia.edu>
2015-01-28 10:48:33 -0500

    Another spec.

--- specs.py
+++ specs.py
@@ -11,3 +11,10 @@ class TestFindQuotedQuotes:
             for m in ps.find_quoted_quotes('She said, "Howdy!" ' * 100)
             ]
         assert quotes == (['"Howdy!"'] * 100), repr(quotes)
+
+    def test_it_should_find_single_quotes(self):
+        quotes = [
+            m.group()
+            for m in ps.find_quoted_quotes("She said, 'Howdy!'")
+            ]
+        assert quotes == ["'Howdy!'"]
