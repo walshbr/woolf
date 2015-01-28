b02695cb5929bd0c20dfc3f584983103806dcfc6
Eric Rochester <erochest@virginia.edu>
2015-01-28 11:16:14 -0500

    Refactored specs with a utility function.

--- specs.py
+++ specs.py
@@ -3,61 +3,52 @@
 import ps
 
 
+def assert_quote(input, expected):
+    quotes = [m.group() for m in ps.find_quoted_quotes(input)]
+    assert expected == quotes, repr(quotes)
+
+
 class TestFindQuotedQuotes:
 
     def test_it_should_find_double_quotes(self):
-        quotes = [
-            m.group()
-            for m in ps.find_quoted_quotes('She said, "Howdy!" ' * 100)
-            ]
-        assert quotes == (['"Howdy!"'] * 100), repr(quotes)
+        assert_quote(
+            'She said, "Howdy!" ' * 100,
+            ['"Howdy!"'] * 100,
+            )
 
     def test_it_should_find_single_quotes(self):
-        quotes = [
-            m.group()
-            for m in ps.find_quoted_quotes("She said, 'Howdy!'")
-            ]
-        assert quotes == ["'Howdy!'"], repr(quotes)
+        assert_quote(
+            "She said, 'Howdy!'",
+            ["'Howdy!'"],
+            )
 
     def test_it_should_find_quotes_with_preceding_contractions(self):
-        quotes = [
-            m.group()
-            for m in ps.find_quoted_quotes("She didn't say, 'Good-bye.'")
-            ]
-        assert quotes == ["'Good-bye.'"], repr(quotes)
+        assert_quote(
+            "She didn't say, 'Good-bye.'",
+            ["'Good-bye.'"],
+            )
 
     def test_it_should_find_quotes_containing_contractions(self):
-        quotes = [
-            m.group()
-            for m in ps.find_quoted_quotes("She said, 'Don't say that!'")
-            ]
-        assert quotes == ["'Don't say that!'"], repr(quotes)
+        assert_quote(
+            "She said, 'Don't say that!'",
+            ["'Don't say that!'"],
+            )
 
     def test_it_should_find_quotes_with_contractions_following(self):
-        quotes = [
-            m.group()
-            for m in ps.find_quoted_quotes("""
-                She said, 'Say that!' Or she didn't.
-                """)
-            ]
-        assert quotes == ["'Say that!'"], repr(quotes)
+        assert_quote(
+            "She said, 'Say that!' Or she didn't.",
+            ["'Say that!'"],
+            )
 
     def test_it_should_find_quotes_surrounded_by_contractions(self):
-        quotes = [
-            m.group()
-            for m in ps.find_quoted_quotes("""
-                She didn't said, 'Don't say that!' Or she didn't.
-                """)
-            ]
-        assert quotes == ["'Don't say that!'"], repr(quotes)
+        assert_quote(
+            "She didn't said, 'Don't say that!' Or she didn't.",
+            ["'Don't say that!'"],
+            )
 
     def test_it_should_find_multiple_quotes_containing_contractions(self):
-        quotes = [
-            m.group()
-            for m in ps.find_quoted_quotes(
-                "She didn't say, 'Don't say that!' "
-                "She didn't say, 'Don't say that!' Something else here."
-                )
-            ]
-        assert quotes == ["'Don't say that!'", "'Don't say that!'"], \
-            repr(quotes)
+        assert_quote(
+            "She didn't say, 'Don't say that!' "
+            "She didn't say, 'Don't say that!' Something else here.",
+            ["'Don't say that!'", "'Don't say that!'"],
+            )
