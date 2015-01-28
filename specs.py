c64d08e07bd97ce7eb783363895e86a28ab8921d
Eric Rochester <erochest@virginia.edu>
2015-01-28 10:56:04 -0500

    Breaking specs.

--- specs.py
+++ specs.py
@@ -18,3 +18,35 @@ class TestFindQuotedQuotes:
             for m in ps.find_quoted_quotes("She said, 'Howdy!'")
             ]
         assert quotes == ["'Howdy!'"]
+
+    def test_it_should_find_quotes_with_preceding_contractions(self):
+        quotes = [
+            m.group()
+            for m in ps.find_quoted_quotes("She didn't say, 'Good-bye.'")
+            ]
+        assert quotes == ["'Good-bye.'"]
+
+    def test_it_should_find_quotes_containing_contractions(self):
+        quotes = [
+            m.group()
+            for m in ps.find_quoted_quotes("She said, 'Don't say that!'")
+            ]
+        assert quotes == ["'Don't say that!'"]
+
+    def test_it_should_find_quotes_with_contractions_following(self):
+        quotes = [
+            m.group()
+            for m in ps.find_quoted_quotes("""
+                She said, 'Say that!' Or she didn't.
+                """)
+            ]
+        assert quotes == ["'Say that!'"]
+
+    def test_it_should_find_quotes_surrounded_by_contractions(self):
+        quotes = [
+            m.group()
+            for m in ps.find_quoted_quotes("""
+                She didn't said, 'Don't say that!' Or she didn't.
+                """)
+            ]
+        assert quotes == ["'Don't say that!'"]
