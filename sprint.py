075c40cfef24a0d966d66c8c12ad3c2fc9ee72bd
Eric Rochester <erochest@virginia.edu>
2015-09-14 13:36:39 -0400

    Trying to shut my lint-checker up just a little.

--- sprint.py
+++ sprint.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 
+"""This trains a naive Bayesian classifier on a corpus with "silent" quotations
+marked. It then tests that classifier for accuracy and prints that metric
+out."""
+
+
 import nltk
 import nltk.corpus
 from nltk import word_tokenize
 from nltk.corpus import names
 from nltk.corpus import brown
 from collections import deque
-import random
+# import random
 
 
 TAGGED = 'training_passages/tagged.txt'
@@ -16,26 +21,26 @@ TEST_SET_RATIO = 0.2
 
 def tokenize_corpus(filename):
     """Read the corpus into test and training sets."""
-    with open(filename) as f:
+    with open(filename) as fin:
         # TODO: word_tokenize isn't breaking carots off from the words.
-        return word_tokenize(f.read())
+        return word_tokenize(fin.read())
 
 
 def build_trainer(tagged_sents, default_tag='NN'):
     """This builds a tagger from a corpus."""
     name_tagger = [nltk.DefaultTagger('PN').tag(names.words())]
 
-    t0 = nltk.DefaultTagger(default_tag)
-    t1 = nltk.UnigramTagger(tagged_sents, backoff=t0)
-    t2 = nltk.BigramTagger(tagged_sents, backoff=t1)
-    t3 = nltk.UnigramTagger(name_tagger, backoff=t2)
+    tagger0 = nltk.DefaultTagger(default_tag)
+    tagger1 = nltk.UnigramTagger(tagged_sents, backoff=tagger0)
+    tagger2 = nltk.BigramTagger(tagged_sents, backoff=tagger1)
+    tagger3 = nltk.UnigramTagger(name_tagger, backoff=tagger2)
 
-    return t3
+    return tagger3
 
 
-def is_verb(tagged_word, context):
+def is_verb(tagged_word, _):
     """This returns True if the tagged word is any form of verb, but
-    it ignores the rest of the context."""
+    it ignores the rest of the context (the second parameter)."""
     (_, tag) = tagged_word
     return tag.startswith('VB')
 
@@ -52,12 +57,13 @@ def windows(seq, window_size):
 
 
 def is_quote(tagged_token):
+    """Is the tagged token a double-quote character?"""
     (word, _) = tagged_token
     return word in {"''", "``", '"', "^"}
 
 
-def is_word(tagged_token, context):
-    """Is the target a word?"""
+def is_word(tagged_token, _):
+    """Is the target a word? This ignores the context of the token."""
     return tagged_token[0].isalnum()
 
 
@@ -82,6 +88,7 @@ def quote_features(tagged_tokens, is_target=is_verb, is_context=is_quote,
 
 
 def main():
+    """The main function."""
     tokens = tokenize_corpus(TAGGED)
     tagger = build_trainer(brown.tagged_sents(categories='news'))
     tagged_tokens = tagger.tag(tokens)
