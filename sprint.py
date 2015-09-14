fbbf966ba6095a25354ee92fc708e40a872674bf
Eric Rochester <erochest@virginia.edu>
2015-09-14 13:17:46 -0400

    Bayes. Naively.

--- sprint.py
+++ sprint.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+
+
 import nltk
 import nltk.corpus
 from nltk import word_tokenize
 from nltk.corpus import names
 from nltk.corpus import brown
 from collections import deque
-from pprint import pprint
+import random
 
 
 TAGGED = 'training_passages/tagged.txt'
@@ -49,41 +52,53 @@ def windows(seq, window_size):
 
 def is_quote(tagged_token):
     (word, _) = tagged_token
-    return word in {"''", "``", '"'}
+    return word in {"''", "``", '"', "^"}
 
 
-def train_quotes(tagged_tokens, is_target=is_verb, is_context=is_quote,
-                 amount_of_context=5):
+def is_word(tagged_token, context):
+    """Is the target a word?"""
+    return tagged_token[0].isalnum()
+
+
+def quote_features(tagged_tokens, is_target=is_verb, is_context=is_quote,
+                   amount_of_context=5, feature_history=0):
     """This returns a conditional frequency distribution for tagged
     tokens for which is_target returns True, with the context being
     determined by is_context and the amount_of_context."""
-
-    cfd = nltk.ConditionalFreqDist()
 
     for chunk in windows(tagged_tokens, 2 * amount_of_context):
         current = chunk[amount_of_context]
         if is_target(current, chunk):
             in_context = any(is_context(t) for t in chunk)
-            cfd[in_context][current[0]] += 1
-
-    return cfd
-
-
-def top_probs(fd, sample=None):
-    """This returns the top probabilities of the frequency
-    distribution. If given `sample` is the number of items to
-    return."""
-    N = float(fd.N())
-    return [(item, n/N) for (item, n) in fd.most_common(sample)]
+            features = {}
+            for offset in range(feature_history+1):
+                history = chunk[amount_of_context - offset]
+                features['token{}'.format(offset)] = history[0]
+                features['tag{}'.format(offset)] = history[1]
+            yield (features, in_context)
 
 
 def main():
     tokens = tokenize_corpus(TAGGED)
     tagger = build_trainer(brown.tagged_sents(categories='news'))
     tagged_tokens = tagger.tag(tokens)
-    trained = train_quotes(tagged_tokens)
-    pprint(top_probs(trained[True], 10))
-    pprint(top_probs(trained[False], 10))
+
+    # Identifying the features.
+    training_features = list(quote_features(
+        tagged_tokens,
+        is_target=is_word,
+        feature_history=4,
+        ))
+
+    # Dividing features into test and training sets.
+    # random.shuffle(training_features)
+    test_size = int(TEST_SET_RATIO * len(training_features))
+    test_set = training_features[:test_size]
+    training_set = training_features[test_size:]
+
+    # stay classy
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+    print(nltk.classify.accuracy(classifier, test_set))
 
 
 if __name__ == '__main__':
