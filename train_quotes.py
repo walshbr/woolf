8a3935bd458caed8fa6e1bc75afebfb1ea8d7356
Eric Rochester <erochest@virginia.edu>
2015-10-21 10:33:25 -0400

    Pulled the process into an abstract class.
    Different arrangements and models of feature sets can be tried by
    subclassing the abstract class.

--- train_quotes.py
+++ train_quotes.py
@@ -18,7 +18,6 @@
 
 
 import argparse
-from collections import deque, namedtuple
 import csv
 import itertools
 from multiprocessing.pool import Pool
@@ -28,125 +27,24 @@ import pickle
 import random
 import statistics
 import sys
-import re
 
 import nltk
 import nltk.corpus
-from nltk.corpus import names
-from nltk.corpus import brown
+
+from fset_manager import Current, TAGGED
 
 
-TAGGED = 'training_passages/tagged_text/'
 TEST_SET_RATIO = 0.2
 
 
-FeatureContext = namedtuple('FeatureContext',
-                            ['history', 'current', 'lookahead'])
-TaggedToken = namedtuple('TaggedToken', ['token', 'tag', 'start', 'end'])
-
 first = operator.itemgetter(0)
 second = operator.itemgetter(1)
-
-
-# [((TOKEN, TAG), (START, END))] -> FeatureContext
-def make_context(window):
-    """This makes a FeatureContext from a window of tokens (which will
-    become TaggedTokens.)"""
-    return FeatureContext(
-        [tagged_token(t) for t in window[:-2]],
-        tagged_token(window[-2]),
-        tagged_token(window[-1]),
-    )
-
-
-def tagged_token(token_span):
-    """This takes an input of ((TOKEN, TAG), (START, END)) and returns
-    a TaggedToken."""
-    ((token, tag), (start, end)) = token_span
-    return TaggedToken(token, tag, start, end)
-
-
-def tokenize_corpus(corpus):
-    """Read the corpus a list sentences, each of which is a list of
-    tokens and the spans in which they occur in the text."""
-    if os.path.isdir(corpus):
-        corpus_dir = corpus
-        corpus = [
-            os.path.join(corpus_dir, fn) for fn in os.listdir(corpus_dir)
-        ]
-    else:
-        corpus = [corpus]
-
-    tokenizer = nltk.load('tokenizers/punkt/{0}.pickle'.format('english'))
-
-    for filename in corpus:
-        with open(filename) as fin:
-            data = fin.read()
-
-        for start, end in tokenizer.span_tokenize(data):
-            sent = data[start:end]
-            sent_tokens = []
-            matches = re.finditer(r'\w+|[\'\"\/^/\,\-\:\.\;\?\!\(0-9]', sent)
-            for match in matches:
-                mstart, mend = match.span()
-                sent_tokens.append(
-                    (match.group(0).lower().replace('_', ''),
-                     (mstart+start, mend+start))
-                    )
-            yield sent_tokens
-
-
-def build_trainer(tagged_sents, default_tag='DEFAULT'):
-    """This builds a tagger from a corpus."""
-    name_tagger = [
-        nltk.DefaultTagger('PN').tag([name.lower() for name in names.words()])
-    ]
-    punctuation_tags = [[('^', '^'), ('"', '"')]]
-    patterns = [
-        (r'.*ing$', 'VBG'),               # gerunds
-        (r'.*ed$', 'VBD'),                # simple past
-        (r'.*es$', 'VBZ'),                # 3rd singular present
-        (r'.*ould$', 'MD'),               # modals
-        (r'.*\'s$', 'NN$'),               # possessive nouns
-        (r'.*s$', 'NNS'),                 # plural nouns
-        (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),  # cardinal numbers
-        (r'.*ly$', 'RB'),                       # adverbs
-        # comment out the following line to raise to the surface all
-        # the words being tagged by this last, default tag when you
-        # run debug.py.
-        (r'.*', 'NN')                     # nouns (default)
-    ]
-
-    # Right now, nothing will get to the default tagger, because the
-    # regex taggers last pattern essentially acts as a default tagger,
-    # tagging everything as NN.
-    tagger0 = nltk.DefaultTagger(default_tag)
-    regexp_tagger = nltk.RegexpTagger(patterns, backoff=tagger0)
-    punctuation_tagger = nltk.UnigramTagger(
-        punctuation_tags, backoff=regexp_tagger
-    )
-    tagger1 = nltk.UnigramTagger(tagged_sents, backoff=punctuation_tagger)
-    tagger2 = nltk.BigramTagger(tagged_sents, backoff=tagger1)
-    tagger3 = nltk.UnigramTagger(name_tagger, backoff=tagger2)
-
-    return tagger3
 
 
 def is_verb(context):
     """This returns True if the tagged word is any form of verb, but
     it ignores the rest of the context (the second parameter)."""
     return context.current.tag.startswith('VB')
-
-
-# [x] -> Int -> [[x]]
-def windows(seq, window_size):
-    """This iterates over window_size chunks of seq."""
-    window = deque()
-    for item in seq:
-        window.append(item)
-        if len(window) > window_size:
-            window.popleft()
-        yield list(window)
 
 
 def is_quote(context):
@@ -157,53 +55,6 @@ def is_quote(context):
 def is_word(context):
     """Is the target a word? This ignores the context of the token."""
     return context.current.token.isalnum()
-
-
-# FeatureContext -> dict
-def get_features(context):
-    """This returns the feature set for the data in the current window."""
-
-    featureset = {
-        'token0': context.current[0],
-        'tag0': context.current[1],
-    }
-    history = reversed(list(context.history))
-    for (offset, (token, tag, _start, _end)) in enumerate(history):
-        featureset['token{}'.format(offset+1)] = token
-        featureset['tag{}'.format(offset+1)] = tag
-
-    return featureset
-
-
-# dict -> FeatureContext -> Bool
-def get_tag(_features, context, is_context=is_quote):
-    """This returns the tag for the feature set to train against.
-    """
-    return is_context(context)
-
-
-# [((TOKEN, TAG), (START, END))]
-# -> [(FEATURES :: dict, SPAN :: (Int, Int), TAG :: Bool)]
-def get_training_features(tagged_tokens, is_target=is_verb,
-                          is_context=is_quote, feature_history=0):
-    """This returns a sequence of feature sets and tags to train against for
-    the input tokens."""
-    window_size = feature_history + 2
-    for window in windows(tagged_tokens, window_size):
-        # window :: [((TOKEN, TAG), (START, END))]
-        if len(window) < 2:
-            continue
-        # make sure that make_context and get_features can work
-        # context :: FeatureContext
-        context = make_context(window)
-        if is_target(context):
-            # features :: dict
-            features = get_features(context)
-            # span :: (Int, Int)
-            span = (context.current.start, context.current.end)
-            # tag :: Bool
-            tag = get_tag(features, context, is_context)
-            yield (features, span, tag)
 
 
 def produce_confusion_matrix(test_features, classifier):
@@ -269,37 +120,6 @@ def cross_validate_means(accuracies):
         yield (cls, statistics.mean(x for (_, x) in accuracy))
 
 
-# FileName -> [[((TOKEN, TAG), (START, END))]]
-def get_tagged_tokens(corpus=TAGGED, testing=False):
-    """This tokenizes, segments, and tags all the files in a directory."""
-    if testing:
-        # train against a smaller version of the corpus so that it
-        # doesn't take years during testing.
-        tagger = build_trainer(brown.tagged_sents(categories='news'))
-    else:
-        tagger = build_trainer(brown.tagged_sents())
-    tagged_spanned_tokens = []
-    tokens_and_spans = tokenize_corpus(corpus)
-    for sent in tokens_and_spans:
-        to_tag = [token for (token, _) in sent]
-        spans = [span for (_, span) in sent]
-        sent_tagged_tokens = tagger.tag(to_tag)
-        tagged_spanned_tokens.append(list(zip(sent_tagged_tokens, spans)))
-    return tagged_spanned_tokens
-
-
-# [[((TOKEN, TAG), (START, END))]] -> [???]
-def get_all_training_features(tagged_tokens):
-    """This takes tokenized, segmented, and tagged files and gets
-    training features."""
-    training_features = []
-    for sent in tagged_tokens:
-        training_features += get_training_features(
-            sent, is_target=is_word, feature_history=2,
-        )
-    return training_features
-
-
 def get_sets(featuresets, ratio):
     """This breaks a sequence of feature sets into two groups based on
     the ratio."""
@@ -354,7 +174,10 @@ def main():
     """The main function."""
     args = parse_args()
 
-    featuresets = get_all_training_features(get_tagged_tokens(args.corpus))
+    manager = Current(is_quote, is_word)
+    featuresets = manager.get_all_training_features(
+        manager.get_tagged_tokens(args.corpus)
+    )
     featuresets = [(fs, tag) for (fs, _, tag) in featuresets]
     random.shuffle(featuresets)
     test_set, training_set = get_sets(featuresets, args.ratio)
