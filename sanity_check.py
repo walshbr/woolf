2b2e7d25e98f96d9523533fce941a89181e365d4
Eric Rochester <erochest@virginia.edu>
2016-05-03 15:59:11 -0400

    Checking my sanity.
    Full of fail.

---
+++ sanity_check.py
@@ -0,0 +1,29 @@
+
+
+import sys
+
+from fset_manager import Current
+from train_quotes import is_quote, is_word
+
+
+
+def normalize(token):
+    return token.lower().replace('_', '')
+
+
+def main():
+    input_file = sys.argv[1]
+
+    with open(input_file) as fin:
+        input_data = fin.read()
+
+    manager = Current(is_quote, is_word)
+    for sent in manager.tokenize_corpus(input_file):
+        for (token, (start, end)) in sent:
+            expected = input_data[start:end]
+            if normalize(expected) != token:
+                print('"{}" != "{}" ({} - {})'.format(token, expected, start, end))
+
+
+if __name__ == '__main__':
+    main()
