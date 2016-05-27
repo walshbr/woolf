a634956334063d40358b12181e900f3f743d0927
Eric Rochester <erochest@virginia.edu>
2016-05-27 09:44:39 -0400

    Getting my lint checker to STFU.

--- sanity_check.py
+++ sanity_check.py
@@ -1,3 +1,4 @@
+"""Making sure the data is acting the way I expect."""
 
 
 import sys
@@ -6,12 +7,13 @@ from fset_manager import Current
 from train_quotes import is_quote, is_word
 
 
-
 def normalize(token):
+    """normalize the token (lower-case, remove _)."""
     return token.lower().replace('_', '')
 
 
 def main():
+    """main"""
     input_file = sys.argv[1]
 
     with open(input_file) as fin:
@@ -22,7 +24,9 @@ def main():
         for (token, (start, end)) in sent:
             expected = input_data[start:end]
             if normalize(expected) != token:
-                print('"{}" != "{}" ({} - {})'.format(token, expected, start, end))
+                print('"{}" != "{}" ({} - {})'.format(
+                    token, expected, start, end,
+                    ))
 
 
 if __name__ == '__main__':
