0eeb09fcc99542a8047c99bd257326c3c2ff91e2
Brandon Walsh <bmw9t@virginia.edu>
2019-09-07 09:22:36 -0400

    update find_quotations

--- find_quotations.py
+++ find_quotations.py
@@ -1,6 +1,9 @@
 # USAGE
-# put this script in the same folder as your plaintext file your trying
-# to extract quoted speech from.
+# put this script in the same folder as your plaintext file you're trying
+# to extract quoted speech from. Edit line 52 with the name of your file.
+# Run it from the command line as a Python3 program like so -
+# python3 find_quotations.py
+# results will be stored in a results.txt file
 
 import re
 
@@ -26,25 +29,31 @@ def find_quoted_quotes(text):
     quotes. Note: if the number of quotation marks is less than fifty
     it assumes that single quotes are used to designate dialogue."""
     if count_quotation_marks(text) < count_single_quotation_marks(text):
-        splits = re.split(r'((?<!\w)\'.+?\'(?!\w))', text)
+        splits = re.findall(r'((?<!\w)\'.+?\'(?!\w))', text)
     else:
-        splits = re.split(r'("[^"]+")', text)
-
+        splits = re.findall(r'("[^"]+")', text)
     return splits
 
 
 def find_non_anglo_quotes(text):
-    splits = re.split(r'(«[^"]»")', text)
+    splits = re.findall(r'(«.+»)', text)
     return splits
 
 
+def save_results(quotes):
+    with open('results.txt', 'w') as fout:
+        for quote in quotes:
+            fout.write(quote)
+            fout.write('\n\n')
+
+
 def main():
-    # to store the path to the text you're looking at
+    # store the path to the text you're looking at below
     filename = 'YOURTEXT.txt'
     text = read_in_text(filename)
     quotes = find_quoted_quotes(text)
     quotes.extend(find_non_anglo_quotes(text))
-    print(quotes)
+    save_results(quotes)
 
 
 if __name__ == '__main__':
